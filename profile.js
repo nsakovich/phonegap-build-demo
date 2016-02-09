179f96d40330da1bd906f93068c8950ab42b34e9
Nick Sakovich <n.sakovich@gmail.com>
2016-02-09 11:12:42 +0300

    User App Builder saved profile TEST

--- profile.js
+++ profile.js
@@ -718,7 +718,7 @@ var profile = {
       "menu": {
         "controls": [
           {
-            "html": "<ion-header-bar class=\"bar-stable\">\n  <h1 class=\"title\">My App</h1>\n</ion-header-bar>\n<ion-content>\n    <ion-list>\n        <ion-item ui-sref=\"search\" menu-close>\n          <i class=\"fa fa-search\"></i> Поиск на Onliner\n        </ion-item>\n        <ion-item ui-sref=\"main\" menu-close>\n          Main Page\n        </ion-item>\n        <ion-item menu-close>\n          Browse\n        </ion-item>\n        <ion-item menu-close>\n          Playlists\n        </ion-item>\n    </ion-list>\n</ion-content>",
+            "html": "<ion-header-bar class=\"bar-stable\">\n  <h1 class=\"title\">My App</h1>\n</ion-header-bar>\n<ion-content>\n    <ion-list>\n        <ion-item ui-sref=\"search\" menu-close>\n          Поиск на Onliner\n        </ion-item>\n        <ion-item ui-sref=\"main\" menu-close>\n          Main Page\n        </ion-item>\n        <ion-item menu-close>\n          Browse\n        </ion-item>\n        <ion-item menu-close>\n          Playlists\n        </ion-item>\n    </ion-list>\n</ion-content>",
             "id": "html-block-1454602947899",
             "name": "Menu Items",
             "type": "html-block"
